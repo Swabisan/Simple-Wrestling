eb9ea2bf2c64cb2c49b2ecf984fc00a053922348
Huy Nguyen <huy9997@gmail.com>
2019-05-21 23:10:28 -0700

    working on the bugs

--- api/endpoints/coachRegisterTeam.js
+++ api/endpoints/coachRegisterTeam.js
@@ -2,33 +2,32 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 
-router.get("/:id?", function(req, res) {
+router.get("/", function(req, res) {
   db.getAllWrestlers().then(response => {
-    res.json({name: "The Testerinos", roster: response.rows});
+    res.json({ name: "The Testerinos", roster: response.rows });
   });
 });
 
 router.post("/updateTeamroster", function(req, res) {
-  console.log('updateTeamroster')
+  console.log("updateTeamroster");
   const dbArray = req.body;
-  console.log(dbArray)
-  db.deleteWrestlersFromTable()
-    .then(result => {
-      console.log(result, "result");
-      for (let i = 0; i < dbArray.length; i++) {
-        const values = [
-          dbArray[i].name,
-          dbArray[i].dob,
-          dbArray[i].weight,
-          dbArray[i].win,
-          dbArray[i].lost
-        ];
-        console.log(values, "values");
-        db.addWrestler(values).then(addResponse => {
-          res.json(addResponse);
-        });
-      }
-    });
+  console.log(dbArray);
+  db.deleteWrestlersFromTable().then(result => {
+    console.log(result, "result");
+    for (let i = 0; i < dbArray.length; i++) {
+      const values = [
+        dbArray[i].name,
+        dbArray[i].dob,
+        dbArray[i].weight,
+        dbArray[i].win,
+        dbArray[i].lost
+      ];
+      console.log(values, "values");
+      db.addWrestler(values).then(addResponse => {
+        res.json(addResponse);
+      });
+    }
+  });
 });
 
 module.exports = router;
